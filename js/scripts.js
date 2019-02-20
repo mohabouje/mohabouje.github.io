ef407aca860e8c116c51d8b2f89756841db7f26e
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2019-02-20 16:02:44 +0100

    Center the labels.

--- js/scripts.js
+++ js/scripts.js
@@ -97,11 +97,3 @@
     });
 
 })(jQuery);
-
-jQuery(document).ready(function(){
-    jQuery('.skillbar').each(function(){
-        jQuery(this).find('.skillbar-bar').animate({
-            width:jQuery(this).attr('data-percent')
-        },6000);
-    });
-});
